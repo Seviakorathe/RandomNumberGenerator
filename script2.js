2e5023a0016487dc016f2d3c88729133ff47b58c
Seviakorathe <155699422+Seviakorathe@users.noreply.github.com>
2024-02-02 19:00:38 +0300

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,21 @@
+const prompt = require("prompt-sync")();
+
+const target = Math.round(Math.random()* 100);
+
+let guesses = 0;
+console.log(target);
+
+while (true){ 
+  guesses ++;
+  const guess = Number(prompt("Guess the number (0-100): "));
+  if (guess > target){
+    console.log ("Your guess is too high.🤭")
+  } else if (guess < target){
+    console.log("Your guess is too low.🙄")
+  } else {
+    console.log("You guessed it!😁");
+    break
+  }
+}
+
+console.log( "You guessed the number in" ,  guesses, "tries. 🥳");
